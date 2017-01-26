f0377ebbab9e4d141dd9b36f212a174a1784d26b
yiwei <yiwei.ge@spaceconcordia.ca>
2017-01-26 01:19:46 -0500

    modify port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.SET.PORT||3000;
+const port = process.env.PORT||3000;
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
 hbs.registerHelper('getCurrentYear',()=>new Date().getFullYear());
